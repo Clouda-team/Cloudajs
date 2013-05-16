17de8499c2bd61d4c11a3f257f3ede8c06643310
tongyao <kirkcolors@gmail.com>
2013-05-16 15:17:43 +0800

    Update sumeru.js

--- app/config/sumeru.js
+++ app/config/sumeru.js
@@ -1,14 +1,14 @@
 //server side config
-var socketPort = 
+/*var socketPort = 
     (typeof process !== 'undefined' && 
      typeof process.BAE !== 'undefined') ?
     80 : 8082;
 var clientSocketServer = typeof location !== 'undefined' ? 
         location.hostname + ':' + socketPort + '/socket/' : '';
-
+*/
 sumeru.config({
-   httpServerPort: 8080,
-   sumeruPath: '/../sumeru',
-   soketPort: socketPort,
-   clientSocketServer : clientSocketServer
+   //httpServerPort: 8080,
+   sumeruPath: '/../sumeru'
+   //soketPort: socketPort,
+   //clientSocketServer : clientSocketServer
 });
